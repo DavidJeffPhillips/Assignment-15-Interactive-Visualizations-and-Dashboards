cb2bc816fc5d84330883f0fb75188aaafc4e9054
David <42821567+DavidJeffPhillips@users.noreply.github.com>
2019-06-06 11:59:23 -0500

    functioning charts, dynamic based on dropdown selection

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,63 +1,64 @@
 function buildMetadata(sample) {
-
+  selected = d3.select("#selDataset").node().value;
   // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-var metadata = d3.json('/metadata/<sample>');
-    // Use d3 to select the panel with id of `#sample-metadata`
-d3.select('#sample-metadata')
-    // Use `.html("") to clear any existing metadata
-  .html("").then(function(data) {
-    console.log(data);
-    for (let [key, value] of Object.entries(sample_metadata)) {
-      console.log(`${key}: ${value}`);
-      d3.append([key, value])
-    };
-  });
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  d3.json(`/metadata/${selected}`).then((metadata) =>
+    d3.select('#sample-metadata')
+      .html('')
+      .html(Object.entries(metadata)
+    )
+  );
 };
+
+
+// Use `d3.json` to fetch the metadata for a sample
+// Use d3 to select the panel with id of `#sample-metadata`
+// Use `.html("") to clear any existing metadata
+// Use `Object.entries` to add each key and value pair to the panel
+// Hint: Inside the loop, you will need to use d3 to append new
+// tags for each key-value in the metadata.
+
+
+// BONUS: Build the Gauge Chart
+// buildGauge(data.WFREQ);
 
 function buildCharts(sample) {
-
+  selected = d3.select("#selDataset").node().value;
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-var sampleData = d3.json('samples/<sample>');
-    // @TODO: Build a Bubble Chart using the sample data
-var bubbleTrace = {
-  x: [sampleData.otu_ids],
-  y: [sampleData.sample_values],
-  mode: "markers",
-  marker: {
-    size: [sampleData.sample_values],
-    color: [sampleData.otu_ids],
-  }
-};
-var bubbleData = [bubbleTrace];
-var bubbleLayout = {
-  title: 'Bubble Title',
-  showlegend: true,
-  height: 500,
-  width: 500
-};
-Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+  // @TODO: Build a Bubble Chart using the sample data
+  d3.json(`/samples/${selected}`).then((sampleData) => {
+    var bubbleTrace = {
+      x: sampleData.otu_ids,
+      y: sampleData.sample_values,
+      mode: "markers",
+      marker: {
+        size: sampleData.sample_values,
+        color: sampleData.otu_ids
+      }
+    };
+    var bubbleData = [bubbleTrace];
+    var bubbleLayout = {
+      title: 'Bubble Title',
+      showlegend: false,
+      height: 500,
+      width: 1000
+    };
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
     // @TODO: Build a Pie Chart
-var pieData = [{
-  values: [sampleData.sample_values.slice(0,10)],
-  labels: [sampleData.otu_ids.slice(0,10)],
-  type: pie
-}];
-var pieLayout = {
-  height: 500,
-  width: 500
-};
-Plotly.newPlot('pie', pieData, pieLayout);
+    var pieData = [{
+      values: sampleData.sample_values.slice(1,10),
+      labels: sampleData.otu_ids.slice(1,10),
+      type: 'pie'
+    }];
+    var pieLayout = {
+      height: 500,
+      width: 500
+    };
+  Plotly.newPlot('pie', pieData, pieLayout);
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  });
 };
 
 function init() {
